d2ee00ba9b4983546d819f88785aeefa813dc845
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-10-02 18:54:21 +0800

    setup api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,7 +8,9 @@ use App\Observers\CouncilObserver;
 use App\Observers\UserObserver;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\Helpers\ApiResponse;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +26,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Response::macro('success', function ($data = [], $message = 'Success', $code = 200) {
+            return ApiResponse::success($data, $message, $code);
+        });
+    
+        // Error macro
+        Response::macro('error', function ($message = 'Error', $code = 400) {
+            return ApiResponse::error($message, $code);
+        });
+    
+        // Paginated macro
+        Response::macro('paginated', function (LengthAwarePaginator $data, $message = 'Data retrieved successfully', $code = 200) {
+            return ApiResponse::paginated($data, $message, $code);
+        });
         User::observe(UserObserver::class);
         Council::observe(CouncilObserver::class);
     }
