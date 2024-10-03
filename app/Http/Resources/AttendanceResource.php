01dfdf87df9fa1ba28c4367b31271b3979d463fd
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-10-03 09:46:29 +0800

    finbakzue evenbt and attendac eresoruce

--- app/Http/Resources/AttendanceResource.php
+++ app/Http/Resources/AttendanceResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\CouncilPositionResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AttendanceResource extends JsonResource
@@ -14,6 +15,21 @@ class AttendanceResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'event_id' => $this->event_id,
+            'council_position_id' => $this->council_position_id,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'status' => $this->status,
+            'check_in_time' => $this->check_in_time ? $this->check_in_time->toDateTimeString() : null,
+            'check_out_time' => $this->check_out_time ? $this->check_out_time->toDateTimeString() : null,
+            'device_id' => $this->device_id,
+            'device_name' => $this->device_name,
+            'selfie_image_url' => $this->selfie_image ? url('storage/' . $this->selfie_image) : null, // Provide the URL to the stored selfie image
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+            'council_position' => new CouncilPositionResource($this->whenLoaded('councilPosition')), // Include council position if loaded
+        ];
     }
 }
--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\CouncilResource;
+use App\Http\Resources\AttendanceResource;
+use App\Http\Resources\CouncilPositionResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EventResource extends JsonResource
@@ -14,6 +17,24 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'content' => $this->content,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'radius' => $this->radius,
+            'start_time' => $this->start_time->toDateTimeString(),
+            'end_time' => $this->end_time->toDateTimeString(),
+            'is_active' => $this->is_active,
+            'max_capacity' => $this->max_capacity,
+            'type' => $this->type,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+            'council' => new CouncilResource($this->whenLoaded('council')), // Include council if loaded
+            'council_position' => new CouncilPositionResource($this->whenLoaded('councilPosition')), // Include council position if loaded
+            'attendances' => AttendanceResource::collection($this->whenLoaded('attendances')), // Include attendances if loaded
+        ];
     }
 }
