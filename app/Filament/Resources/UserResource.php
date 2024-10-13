02fd71e29b563842c766c0a36d10bc72baf6599c
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-10-13 18:29:48 +0800

    wip

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -69,6 +69,14 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
+                ImageColumn::make('image')
+                ->disk('public')
+                ->label('Image')
+                ->width(60)->height(60)
+                ->url(fn (User $record): null|string => $record->profile_photo_path ?  Storage::disk('public')->url($record->profile_photo_path) : null)
+                ->defaultImageUrl(url('/images/placeholder-image.jpg'))
+                ->openUrlInNewTab()
+                ->circular(),
                 Tables\Columns\TextColumn::make('first_name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('last_name')
