5d0b1cf489954cd157326b27325650f175408a9f
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-11-11 19:48:33 +0800

    wip

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class NotificationResource extends JsonResource
@@ -14,6 +15,18 @@ class NotificationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $createdAt = $this->created_at ? Carbon::parse($this->created_at)->format('M d, Y h:i:s A') : null;
+        $updatedAt = $this->updated_at ? Carbon::parse($this->updated_at)->format('M d, Y h:i:s A') : null;
+        $readAt = $this->read_at ? Carbon::parse($this->read_at)->format('M d, Y h:i:s A') : null;
+
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            
+            'data' => $this->data ? json_decode($this->data, true) : null,
+            'read_at' => $readAt,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt,
+        ];
     }
 }
