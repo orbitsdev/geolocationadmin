8cffe02348304819dc4f36668a657e096b2aba87
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-11-16 20:23:42 +0800

    wip

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -13,11 +13,18 @@ class Event extends Model
     use HasFactory;
 
     protected $casts = [
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'radius' => 'float',        
+        'is_active' => 'boolean',
+        'restrict_event' => 'boolean',
         'start_time' => 'datetime',
         'end_time' => 'datetime',
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
+    
+    
     public function councilPosition()
     {
         return $this->belongsTo(CouncilPosition::class);
