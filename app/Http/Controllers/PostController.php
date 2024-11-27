b0ac065fb7c81fb76340b8e70a507a1ab3934f2b
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-11-27 13:44:34 +0800

    wip

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -106,6 +106,8 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
 {
+
+    return ApiResponse::success($request->all());
     $post = Post::findOrFail($id);
 
     // Validate the incoming request
@@ -114,7 +116,7 @@ class PostController extends Controller
         'title' => 'sometimes|string|max:255',
         'content' => 'sometimes|string',
         'description' => 'nullable|string',
-    'is_publish' => 'required|in:true,false,1,0',
+        'is_publish' => 'required|in:true,false,1,0',
 
         'media.*' => ['nullable', 'file', 'mimes:jpeg,png,mp4', 'max:50480'],
     ]);
