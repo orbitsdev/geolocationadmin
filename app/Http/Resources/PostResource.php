e3d1c69db025b725f028c905e88a5d132497cefe
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-11-27 09:05:05 +0800

    wip

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -23,7 +23,7 @@ class PostResource extends JsonResource
             'title' => $this->title,
             'content' => $this->content,
             'description' => $this->description,
-           
+            'is_publish' => $this->is_publish,
             'created_at' => $this->created_at ? $this->created_at->format('F j, Y, g:i A') : null,
             'updated_at' => $this->updated_at ? $this->updated_at->diffForHumans() : null,
             'council' => new CouncilResource($this->whenLoaded('council')),
@@ -36,19 +36,19 @@ class PostResource extends JsonResource
                     'file_name' => $media->file_name,
                     'url' => $media->getUrl(),
                     'type' => $media->mime_type,
-                    'extension' => $extension, 
+                    'extension' => $extension,
                 ];
             }),
             'related_model' => $this->postable ? [
                 'type' => class_basename($this->postable_type), // Extract the model name (e.g., 'Event', 'Collection')
                 'id' => $this->postable_id, // The ID of the related model
-                'data' => $this->postable instanceof Event 
-                    ? new EventResource($this->postable) 
-                    : ($this->postable instanceof Collection 
-                        ? new CollectionResource($this->postable) 
+                'data' => $this->postable instanceof Event
+                    ? new EventResource($this->postable)
+                    : ($this->postable instanceof Collection
+                        ? new CollectionResource($this->postable)
                         : null),
             ] : null,
-           
+
         ];
     }
 }
