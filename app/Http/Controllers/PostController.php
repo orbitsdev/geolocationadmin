526ef8517809eb64cd54640866d9f431166f755f
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-11-28 13:16:42 +0800

    wip

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -105,8 +105,9 @@ class PostController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-{
+{   
 
+    return ApiResponse::success($request->all());
     
     $post = Post::findOrFail($id);
 
