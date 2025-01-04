cab15a36a6146d5c84cd80dd1b2eafa450655312
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-04 09:06:00 +0800

    wip

--- resources/views/exports/collections-by-council.blade.php
+++ resources/views/exports/collections-by-council.blade.php
@@ -1,7 +1,7 @@
 <table align="left">
     <thead>
         <tr style="background-color: #106c3b; color: white">
-            <th style="background-color: #106c3b; color: white;">Collection ID</th>
+            {{-- <th style="background-color: #106c3b; color: white;">Collection ID</th> --}}
             <th style="background-color: #106c3b; color: white;">Title</th>
             <th style="background-color: #106c3b; color: white;">Type</th>
             <th style="background-color: #106c3b; color: white;">Description</th>
@@ -11,8 +11,12 @@
     </thead>
     <tbody>
         @foreach ($collections as $collection)
+            @php
+                // Calculate the total amount for this collection
+                $totalAmount = $collection->collectionItems->sum('amount');
+            @endphp
             <tr>
-                <td align="left" width="40">{{ $collection->id }}</td>
+                {{-- <td align="left" width="40">{{ $collection->id }}</td> --}}
                 <td align="left" width="40">{{ $collection->title }}</td>
                 <td align="left" width="40">{{ $collection->type }}</td>
                 <td align="left" width="40">{{ $collection->description ?? 'N/A' }}</td>
@@ -24,6 +28,9 @@
                                 {{ $item->label }}: {{ number_format($item->amount, 2) }}
                             </li>
                         @endforeach
+                        <li style="font-weight: bold;">
+                            Total: {{ number_format($totalAmount, 2) }}
+                        </li>
                     </ul>
                 </td>
             </tr>
