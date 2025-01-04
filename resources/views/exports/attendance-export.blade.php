2a8263b9ec46d130ed48cfcc0e6261cb3fab857c
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-04 08:29:16 +0800

    wip

--- resources/views/exports/attendance-export.blade.php
+++ resources/views/exports/attendance-export.blade.php
@@ -3,6 +3,7 @@
         <tr style="background-color: #106c3b; color: white">
             {{-- <th style="background-color: #106c3b; color: white;">ID</th> --}}
             <th style="background-color: #106c3b; color: white;">Name</th>
+            <th style="background-color: #106c3b; color: white;">Position</th>
             <th style="background-color: #106c3b; color: white;">Event Name</th>
             <th style="background-color: #106c3b; color: white;">Time In</th>
             <th style="background-color: #106c3b; color: white;">Time Out</th>
@@ -13,11 +14,16 @@
         @foreach ($attendances as $attendance)
             <tr>
                 {{-- <td align="left" width="40">{{ $attendance->id }}</td> --}}
-                <td align="left" width="40">{{ $attendance->councilPosition->user->fullName()  }}</td>
-                <td align="left" width="40">{{ $event->title }}</td>
-                <td align="left" width="40">{{ $attendance->check_in_time ? $attendance->check_in_time->format('F j, Y, g:i A') : 'N/A' }}</td>
-                <td align="left" width="40">{{ $attendance->check_out_time ? $attendance->check_out_time->format('F j, Y, g:i A') : 'N/A' }}</td>
-                <td align="left" width="40">{{ ucfirst($attendance->status) }}</td>
+                <td align="left" width="40">{{ $attendance?->councilPosition?->user?->fullName() ?? 'N/A' }}</td>
+                <td align="left" width="40">{{ $attendance?->councilPosition?->position ?? 'N/A' }}</td>
+                <td align="left" width="40">{{ $event?->title ?? 'N/A' }}</td>
+                <td align="left" width="40">
+                    {{ $attendance?->check_in_time ? $attendance->check_in_time->format('F j, Y, g:i A') : 'N/A' }}
+                </td>
+                <td align="left" width="40">
+                    {{ $attendance?->check_out_time ? $attendance->check_out_time->format('F j, Y, g:i A') : 'N/A' }}
+                </td>
+                <td align="left" width="40">{{ ucfirst($attendance?->status ?? 'N/A') }}</td>
             </tr>
         @endforeach
     </tbody>
